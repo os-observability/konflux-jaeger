1e4c03c923b9d33526d70e717b71f6498f7b4608
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-10-30 12:47:57 +0000

    Update jaeger-all-in-one to af0f0f2
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -11,7 +11,7 @@ export JAEGER_INGESTER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tena
 # Separate due to merge conflicts
 export JAEGER_QUERY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-query@sha256:bf474bea2818eb2bc246083a671e7f4d9228dd8ef7db3430782c1531a23d5de0"
 # Separate due to merge conflicts
-export JAEGER_ALL_IN_ONE_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-all-in-one@sha256:2ae05b48d25fa20607326b3860a19a2d63affa7afa798464bc4c40bb10974612"
+export JAEGER_ALL_IN_ONE_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-all-in-one@sha256:af0f0f25405c0a0babd72c75da7a409bf44b7e34787323780cc0c2e652c49249"
 # Separate due to merge conflicts
 export JAEGER_ROLLOVER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-es-rollover@sha256:621af47f10d915a7434d1b932a19ef22bab2d8dcdc9749eb3347a84b21dbaf92"
 # Separate due to merge conflicts
